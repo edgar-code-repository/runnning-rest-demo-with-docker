92b5104882b8830bb76b7d99237900b7e7935747
Edgar <ecofre@gmail.com>
2023-03-03 19:29:04 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/RunningRestDemoWithDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningRestDemoWithDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningRestDemoWithDockerApplication.class, args);
+	}
+
+}
